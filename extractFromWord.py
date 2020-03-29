8b515f53f6249371b0f30fb79982aae73fcf39fb
Ryan Johnson <ryan.johnson@squareonesoft.com>
2020-03-29 12:25:55 +0800

    Added output

--- extractFromWord.py
+++ extractFromWord.py
@@ -1,16 +1,22 @@
 from docx import *
 import re
-import json
 
 document = Document('randomNames.docx')
 
-
+emails = []
 for para in document.paragraphs:
     text = para.text
     if "Mother" in text:
         email_list = re.findall(r"[a-z0-9\-+_]+@[a-z0-9\-+_]+\.[a-z]+", text)
         for email in email_list:
-            print(email)
+            emails.append(email)
+
+output = Document()
+for email in emails:
+    p = output.add_paragraph(email)
+output.save('emails.docx')
+
+
 
 
 
